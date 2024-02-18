0538185e7a3f2ebb6d11ce7c4fa467618dd6914f
vsharm <vijayasharma4949@gmail.com>
2024-02-18 10:16:45 -0800

    implements return statement parsing

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -63,6 +63,8 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
     case token.LET:
         return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
     default:
         return nil
     }
@@ -87,6 +89,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
     return stmt
 }
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+    stmt := &ast.ReturnStatement{Token: p.curToken}
+
+    for !p.curTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
+}
+    
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
     return p.curToken.Type == t
--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -18,11 +18,8 @@ let foobar = 838383;
     program := p.ParseProgram();
     checkParserErrors(t, p)
 
-    if program == nil {
-        t.Fatalf("ParseProgram() returned nil")
-    }
     if len(program.Statements) != 3 {
-        t.Fatalf("program.Statements does not contain 4 statements. got=%d",
+        t.Fatalf("program.Statements does not contain 3 statements. got=%d",
             len(program.Statements))
     }
     
@@ -41,6 +38,38 @@ let foobar = 838383;
         }
     }
 }
+
+func TestReturnStatements(t *testing.T) {
+    input := `
+return 5;
+return 10;
+return 993322;
+`
+    
+    l := lexer.New(input)
+    p := New(l)
+
+    program := p.ParseProgram();
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 3 {
+        t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+            len(program.Statements))
+    }
+
+    for _, stmt := range program.Statements {
+        returnStmt, ok := stmt.(*ast.ReturnStatement)
+        if !ok {
+            t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+            continue
+        }
+        if returnStmt.TokenLiteral() != "return" {
+            t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+                returnStmt.TokenLiteral())
+            }
+        }
+    }
+
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
     if s.TokenLiteral() != "let" {
